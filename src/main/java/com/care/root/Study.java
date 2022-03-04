ddcfaecf0f6a4cfefe0d4ae2b7435d6dd72df3b7
yhj <tntj123@naver.com>
2022-03-04 21:18:26 +0900

    part-2

--- src/main/java/com/care/root/Study.java
+++ src/main/java/com/care/root/Study.java
@@ -4,8 +4,20 @@ public class Study {
 	
 	private StudyStatus status = StudyStatus.DRAFT;
 	
+	private int limit;
+	
+	public Study(int limit) {
+		if(limit < 0) {
+			throw new IllegalArgumentException("limit은 0보다 커야함");
+		}
+		this.limit = limit;
+	}
+	
 	public StudyStatus getStatus() {
 		return this.status;
 	}
 
+	public int getLimit() {
+		return limit;
+	}
 }
--- src/test/java/com/care/root/StudyTest.java
+++ src/test/java/com/care/root/StudyTest.java
@@ -1,7 +1,11 @@
 package com.care.root;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -12,20 +16,50 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class) //실행시 underscore을 없애준다. 
 class StudyTest {
 
 	@Test
 	@DisplayName("스터디 만들기") //테스트하는 메소드이름 설정 가능
+	@EnabledOnJre(JRE.JAVA_11) // 해당 jre 에서만 실행
+	@EnabledOnOs(OS.WINDOWS) // 해당 os 에서만 실행
 	void create_new_study() {
-		Study study = new Study();
-		assertNotNull(study);
-		assertEquals(StudyStatus.DRAFT, study.getStatus(),"스터디가 실행되면 처음은 draft여야 한다.");
+		Study study = new Study(1);
+		
+		/*
+		 * 	assumeTrue()
+		 *  처음 조건을 건뒤 조건이 true 이면 테스트를 진행한다.
+		 */
+		
+		//테스트를 한번에 진행시키게 해준다
+		assertAll(
+			()-> assertNotNull(study),	
+			 
+			()-> assertEquals(StudyStatus.DRAFT, study.getStatus(), ()-> "스터디가 실행되면 처음은 draft여야 한다."),
+						//          기대값                            상태값
+			
+			()-> assertTrue(study.getLimit()> 0,"스터디는 0")
+				);
+		
+		//예외발생여부 확인
+		assertThrows(IllegalArgumentException.class, ()-> new Study(2));
+		
+		//메세지 여부 확인
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()-> new Study(1));
+		assertEquals("limit은 0보다 커야함",exception.getMessage());
+	
+		
 	}
 	
 	@Test
 	// @Disabled // 실행하고 싶지 않은 테스트코드에 사용한다.
+	// @EnabledIfEnvironmentVariable(named = "", matches="") 매칭되는 값이 있으면 테스트 실행
 	void create_other_class() {
 		System.out.println("Create1");
 	}
